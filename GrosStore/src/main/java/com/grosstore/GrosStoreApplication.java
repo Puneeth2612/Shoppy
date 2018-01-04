6e92aa0b982f79fb024457262213141f4b953c43
puneeth26 <puneeth26@localhost>
2018-01-04 22:08:32 +0530

    First Commit

---
+++ GrosStore/src/main/java/com/grosstore/GrosStoreApplication.java
@@ -0,0 +1,12 @@
+package com.grosstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrosStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrosStoreApplication.class, args);
+	}
+}
