309b9c740368e9706389ea82112332d163400543
puneeth26 <puneeth26@localhost>
2018-02-20 11:19:05 +0530

    Landing Page

--- GrosStore/src/main/java/com/grosstore/Controller/HomeController.java
+++ GrosStore/src/main/java/com/grosstore/Controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.grosstore.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,9 +11,29 @@ public String index()
 {
 	return "index";
 }
+
+
 @RequestMapping("/login")
-public String login()
+public String login(Model model)
 {
+	model.addAttribute("classActiveLogin",true);
 	return "login";
 }
+
+
+@RequestMapping("/forgetPassword")
+public String forgetPassword(Model model)
+{
+	model.addAttribute("classActiveForgetPassword",true);
+	return "login";
+}
+
+
+@RequestMapping("/newUser")
+public String newUser(Model model)
+{
+	model.addAttribute("classActiveNewUser",true);
+	return "login";
+}
+
 }
